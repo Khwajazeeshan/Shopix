fa17d029811b6b706cdd87469c8807d2a39a4512
khwajazeeshan <khawajazeeshan225@gmail.com>
2026-04-14 13:09:32 +0500

    Ecommerce Project

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,7 +1,11 @@
 import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
-export const pusherServer = new PusherServer({
+// Handle CJS/ESM default import interop
+const PServer = (PusherServer as any).default || PusherServer;
+const PClient = (PusherClient as any).default || PusherClient;
+
+export const pusherServer = new PServer({
   appId: process.env.app_id!,
   key: process.env.key!,
   secret: process.env.secret!,
@@ -9,6 +13,10 @@ export const pusherServer = new PusherServer({
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+// pusher-js (client) often exports the constructor as .Pusher or the default export itself
+const PusherClientConstructor = (PClient as any).Pusher || PClient;
+
+export const pusherClient = new PusherClientConstructor(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
 });
+
